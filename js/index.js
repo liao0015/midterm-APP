312f16cda0c154313fcabc3694871e788a4579f9
BO LIAO <liao0015@algonquinlive.com>
2015-03-27 16:26:10 -0400

    update one

--- js/index.js
+++ js/index.js
@@ -1,94 +1,153 @@
-// JavaScript Document
-var pages = [];
-var links = [];
-var numLinks = 0;
-var numPages = 0;
+var allData = new Object();
+var data = {};
 
-//document.addEventListener("DOMContentLoaded", onDeviceReady, false);
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("DOMContentLoaded", onDeviceReady, false);
+//document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
-		pages = document.querySelectorAll('[data-role="page"]');
-		numPages = pages.length;
-		links = document.querySelectorAll('[data-role="pagelink"]');
-		numLinks = links.length;
-		for(var i = 0; i < numLinks; i++){
-			if(detectTouchSupport()){
-				links[i].addEventListener("touchend", handleTouch, false);
-			}
-			links[i].addEventListener("click", handleLinkClick, false);
-			links[i].addEventListener("click", addContact, false);
+	addHammerEventListener();
+	
+	document.getElementById("btnClose").addEventListener("click", closeModalContact);
+	document.getElementById("btnCloseMap").addEventListener("click", closeModalMap);
+	
+	addContact();
+
+	document.addEventListener("scroll", handleScrolling, false);
+}
+
+
+function addHammerEventListener(ev){
+	var tar = document.querySelector("[data-role=listview]");
+	var mc = new Hammer(tar, {});
+	
+	var singleTap = new Hammer.Tap({event: 'tap'});
+	var doubleTap = new Hammer.Tap({event:'doubletap', taps:2, threshold:10, posThreshold:25});
+	mc.add([doubleTap, singleTap]);
+	
+	doubleTap.requireFailure(singleTap);
+	
+	mc.on("tap", function(ev){
+		document.querySelector("[data-role=modal]").style.display = "block";
+		document.querySelector("[data-role=overlay]").style.display = "block";
+		showInfor(ev);
+	});
+	mc.on("doubletap", function(ev){
+		document.querySelector("[data-role=map]").style.display = "block";
+		document.querySelector("[data-role=overlay]").style.display = "block";
+		getGeolocation(ev);
+	});
+}
+
+//fetch in contacts, convert into JSON object and save in localstorage with the name "data"  
+function addContact(){
+	var options = new ContactFindOptions( );
+	options.filter = ""; 
+	options.multiple = true;
+	var fields = [navigator.contacts.fieldType.displayName];
+	navigator.contacts.find(fields, successFunc, errFunc, options);
+}
+
+function successFunc( matches ){
+	data = {
+	  'contactInfor': [],
+	  'state': true
+	};
+	//read each contact from the contact list and convert each contact from the list into JSON as an object; the complete JSON will be converted into a string and stored in localstorage
+	for (var i = 0; i < matches.length; i++){
+		data.contactInfor.push({ 'id': matches[i].id, 'name': matches[i].displayName, 'homeNum': matches[i].phoneNumbers[0].value, 'mobileNum': matches[i].phoneNumbers[1].value, 'lat': "", 'lng': "" });
+		localStorage.setItem('data', JSON.stringify(data));
+	}
+	//conver the sotred strings in localstorage back into JSON object 
+	allData = JSON.parse(localStorage.getItem('data'));
+	//display 12 contacts on the home screen
+	var ul = document.querySelector("[data-role=listview]");
+	for (var i = 0; i < 13; i++){
+		var li = document.createElement("li");
+		li.setAttribute("data-ref", i);
+		li.innerHTML = "Name: " + allData.contactInfor[i].name;
+		ul.appendChild(li);
+	}
+}
+//display error message if contacts can not be found
+function errFunc(matches){
+	if (matches == null){
+		alert("No contacts!");
+	}
+}
+
+//once single tapped/clicked, show full name and phone numbers of the contact
+function showInfor(e){
+	var x = document.getElementById("bottle");
+	x.innerHTML = "";
+	
+	var p1 = document.createElement("p");
+	var p2 = document.createElement("p");
+	var p3 = document.createElement("p");
+	var num = e.target.getAttribute("data-ref");
+	
+	p1.innerHTML = "Name: "+ allData.contactInfor[num].name;
+	p2.innerHTML = "Home: "+ allData.contactInfor[num].homeNum;
+	p3.innerHTML = "Mobile: "+ allData.contactInfor[num].mobileNum;
+	x.appendChild(p1);
+	x.appendChild(p2);
+	x.appendChild(p3);
+}
+
+//close the contact modal
+function closeModalContact(){
+	document.querySelector("[data-role=modal]").style.display="none";
+	document.querySelector("[data-role=overlay]").style.display="none";
+}
+
+//close the map modal
+function closeModalMap(){
+	document.querySelector("[data-role=map]").style.display="none";
+	document.querySelector("[data-role=overlay]").style.display="none";
+}
+
+function getGeolocation(e){
+	//display the current contact name
+	var y = document.getElementById("bottleMap");
+	y.innerHTML = "";
+	
+	var p = document.createElement("p");
+	var num = e.target.getAttribute("data-ref");
+	p.innerHTML = "Name: "+ allData.contactInfor[num].name;
+	y.appendChild(p);
+	
+	//display dynamic google map centered at Algonquin college
+	var mapOptions ={
+		  center:new google.maps.LatLng(45.348247,-75.756086),
+		  zoom:14,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
+	var map =new google.maps.Map(document.getElementById("box"), mapOptions);
+	var latitude;
+	var longitude;
+	google.maps.event.addListener(map, 'click', function(ev) 
+	{
+		placeMarker(ev.latLng, map);
+		//alert('latitude:'+ev.latLng.lat+'; longitude:'+ev.latLng.lng+';');
+		//console.log(ev.latLng.lat);
+		function placeMarker(position, map) 
+		{
+			var marker = new google.maps.Marker({
+				position: ev.latLng,
+				map: map
+			});
+			map.panTo(position);
 		
-		window.addEventListener("popstate", browserBackButton, false);
-		document.addEventListener("scroll", handleScrolling, false);
-		loadPage(null);
-}
-
-ShowStaticMap();
-
-function handleTouch(ev){
-	ev.preventDefault();
-	ev.stopImmediatePropagation();
-	var touch = ev.changedTouches[0];
-	var newEvt = document.createEvent("MouseEvent");
-	newEvt.initMouseEvent("click", true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY);
-	ev.currentTarget.dispatchEvent(newEvt);
-}
-
-function handleLinkClick(ev){
-	ev.preventDefault();
-	var href = ev.target.href;
-	var parts = href.split("#");
-	loadPage(parts[1]);
-	return false;
-}
-
-function loadPage(url){
-	if(url == null){
-		pages[0].style.display = "block";
-		history.replaceState(null, null, "#home");
-	}
-	else{
-		for (var i =0; i < numPages; i++){
-			if (pages[i].id == url){
-				pages[i].style.display = "block";
-				history.pushState(null, null, "#" + url);
-			}
-			else{
-				pages[i].style.display = "none";
-			}
+			google.maps.event.addListener(marker, "click", function (event) 
+			{
+				latitude = event.latLng.lat();
+				longitude = event.latLng.lng();
+				alert( latitude + ', ' + longitude );
+			});
 		}
-		for (var x = 0; x < numLinks; x++){
-			links[x].className = "";
-			svgs[x].classList.remove("svgShow");
-			if (links[x].href == location.href){
-				links[x].className = "show";
-				svgs[x].classList.add("svgShow");
-			}
-		}
-	}
-}
-
-//Using browser's back button
-function browserBackButton(ev){
-	url = location.hash;
-	for(var i = 0; i < numPages; i++){
-		if(("#" + pages[i].id) == url){
-			pages[i].style.display = "block";
-		}
-		else {
-			pages[i].style.display = "none";
-		}
-	}
-	for(var x = 0; x < numLinks; x++){
-		links[x].className = "";
-		svgs[x].classList.remove("svgShow");
-		if(links[x].href == location.href){
-			links[x].className = "show";
-			svgs[x].classList.add("svgShow");
-		}
-	}
+	});	
+	data.contactInfor[num]({ 'lat': latitude, 'lng': longitude});
+	localStorage.setItem('data', JSON.stringify(data));
+	allData = JSON.parse(localStorage.getItem('data'));
 }
 
 //sticky footer when scrolled down
@@ -96,57 +155,26 @@ function handleScrolling(ev){
 	var height = window.innerHeight;
 	var offset = window.pageYOffset;
 	var footHeight = 60;
-	var footer = document.querySelector("#sticky");
+	var footer = document.querySelector('[data-role="footer"]');
 	footer.style.position = "absolute";
 	var total = height + offset - footHeight;
 	footer.style.top = total + "px";
 }
 
-//test for browers support of touch events
-function detectTouchSupport(){
-	msGesture = navigator && navigator.msPointerEnabled && navigator.msMaxTouchPoints > 0 && MSGesture;
-	var touchSupport = (("ontouchstart" in window) || msGesture || (window.DocumentTouch && document instanceof DocumentTouch));
-	return touchSupport;
-}
-
-//geolocation showing goodle static map of current location
-function ShowStaticMap(){
-	//check if geolocation is supported
-	if( navigator.geolocation ){ 
-	
-		var params = {enableHighAccuracy: true, timeout:360000, maximumAge:0};
-		navigator.geolocation.getCurrentPosition( watchPosition, gpsError, params ); 
-		
-		function watchPosition( position ){ 
-			//create canvas element and append into div
-			var x = document.getElementById("box");
-			var canvas = document.createElement("canvas");
-			canvas.width = "310";
-			canvas.height = "310";
-			canvas.style.border = "2px dashed teal";
-			var context = canvas.getContext("2d");
-			x.appendChild(canvas);
-			
-			//get location parameters and display map img in canvas
-			var lat = position.coords.latitude;
-			var lon = position.coords.longitude;
-			var img = document.createElement("img");
-			img.src = "https://maps.googleapis.com/maps/api/staticmap?&size=400x400&markers=color:red%7C" + lat + "," + lon;
-			img.onload = function() {
-    			context.drawImage(img, 0, 0);
-  			};
-		}
-		
-		function gpsError( error ){   
-  			var errors = {
-			1: 'Permission denied',
-			2: 'Position unavailable',
-			3: 'Request timeout'
-  			};
-  			alert("Error: " + errors[error.code]);
+//the back button
+/*function goBack(){
+	console.log("back button function needed");
+	if(url == null){
+		pages[0].style.display = "block";
+		hitory.replaceState(null, null, "#home");
+	}else{
+		for(var i =0; i < numPages; i++){
+			if(pages[i].id ==url){
+				pages[i].style.display = "block";
+				history.pushState(null,null,"#", + url);
+			}else{
+				pages[i].style.display = "none";
+			}
 		}
 	}
-	else{
-		alert("Your browser doesn't support geolocation function.");
-	}
-}
+}*/
